ea4162d7cb97f7e98424b3435208c3716b2e4fc6
Rauf010 <9014468@student.zadkine.nl>
2022-10-12 11:55:19 +0200

    p1-javascript_opdr5.1

---
+++ Opdr_js_5.1/script.js
@@ -0,0 +1,45 @@
+alert("hello wereld!:");
+
+
+
+document.getElementById("title").style.color = "red";
+
+
+
+var getal1 = 12;
+
+var getal2 = 4;
+
+var som = getal1 * getal2;
+
+alert("het antwoord is: " + som)
+
+// promptbox
+
+
+
+var kleur = prompt("Geef een Engelse kleurnaam: (red of blue", "");
+
+
+
+
+
+//kleur aanpassen
+
+
+
+document.getElementById("title").style.color = "kleur";
+
+
+
+//document.getElementById("title").style.color = "red";
+
+
+
+
+
+//tekstaanpassen
+
+
+
+document.getElementById("p1").innerHTML = "Hello "+ kleur +"!";
