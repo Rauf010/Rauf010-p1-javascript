7c731c5c4270c4efdf4395d9d0678003751ed42e
Rauf010 <9014468@student.zadkine.nl>
2022-11-03 09:44:57 +0100

    P1-javascript

---
+++ Opdr_js_6.4/Opdr_6.4.js
@@ -0,0 +1,17 @@
+function calculator(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer = 13);
+    alert("13");
+}
+
+function MultiplyByFive() {
+    getal1 = prompt("geef een getal");
+    let answer = getal1 * 5; 
+    console.log(answer);
+}
+
+function calculateMinutes() {
+    getal1 = prompt("geef een getal");
+    let answer = getal1 / 60;
+    console.log(answer);
+}
---
+++ Opdr_js_7.1/Opdr_7.1.js
@@ -0,0 +1,8 @@
+let budget = 100;
+let product = 60;
+
+
+if (product > budget) {
+    document.getElementById("h1").innerHTML = 'helaas,te weinig geld'
+}
+
